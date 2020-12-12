f61e496a032d19e512224b28821b626a08fe022c
Armir <armirzogu97@gmail.com>
2020-12-12 12:37:38 +0100

    Added Person

--- models/personi.rb
+++ models/personi.rb
@@ -5,3 +5,23 @@
 	dhe vendbanimi, numri i telefonit që duhet të kenë hapësirë për modifikime. Të mbishkruhet metoda
 	to_s e cila duhet të jap të gjitha të dhënat e personit përkatës. 
 =end
+class Person
+	attr_reader :emri, :mbiemri, :ditelindja, :numri_personal
+	attr_accessor :vendbanimi, :numri_tel 
+
+	def initialize(emri,mbiemri,ditelindja,nr_personal,vendbanimi,numri_tel)
+		@emri=emri
+		@mbiemri=mbiemri
+		@ditelindja=ditelindja
+		@nr_personal=nr_personal
+		@vendbanimi=vendbanimi
+		@numri_tel=numri_tel
+		
+	end
+	def to_s
+		"Te dhenat e personit jane: Emri:#{@emri},Mbiemri:#{@mbiemri}, Ditelindja: #{@ditelindja}, Numri personal: #{@nr_personal}, vendbanimi: #{@vendbanimi}, Numri telefonit: #{@numri_tel}"
+
+	end
+end
+	klasa=Person.new("Armir","Zogu","19.02.1997","1241896648","Prishtine","44958107")
+	puts klasa
